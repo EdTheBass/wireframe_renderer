8adb0adc7af2d1b78de8b794ed6cfa5c07a228f3
EdTheBass <68122094+EdTheBass@users.noreply.github.com>
2022-12-14 20:10:55 +0000

    Add files via upload

---
+++ wireframe_renderer.py
@@ -0,0 +1,118 @@
+import pygame as game
+import numpy as np
+
+game.init()
+
+WIDTH = 800
+HEIGHT = 800
+
+screen = game.display.set_mode((WIDTH, HEIGHT))
+game.display.set_caption("Wireframe Renderer")
+
+# ============================= CUBE ============================= 
+vertices = [
+    [-200, 200, -200], 
+    [-200, 200, 200], 
+    [-200, -200, -200], 
+    [-200, -200, 200],
+    [200, 200, -200], 
+    [200, 200, 200], 
+    [200, -200, -200], 
+    [200, -200, 200],]
+lines = [[0, 1], [0, 2], [1, 3], [2, 3], [4, 5], [4, 6], [5, 7], [6, 7], [2, 6], [1, 5], [3, 7], [0, 4]]
+
+# ============================= PYRAMID =============================
+# vertices = [
+#     [-200, -200, -200], 
+#     [-200, -200, 200],
+#     [200, -200, -200],
+#     [200, -200, 200],
+#     [0, 200, 0]
+# ]
+# lines = [[0, 1], [0, 2], [1, 3], [2, 3], [0, 4], [1, 4], [2, 4], [3, 4]]
+
+# ============================= TETRAHEDRON =============================
+# vertices = [
+#     [200, -200, -100 * np.sqrt(3)],
+#     [-200, -200, -100 * np.sqrt(3)],
+#     [0, -200, 100 * np.sqrt(3)],
+#     [0, 200, (-100 * np.sqrt(3))/3]
+# ]
+# lines = [[0, 1], [0, 2], [1, 2], [0, 3], [1, 3], [2, 3]]
+
+
+p_vertices = []
+
+focal_len = 1000
+
+def project(x, y, z, f):
+    xr = (x * f) / (f + z)
+    yr = (y * f) / (f + z)
+    return [(WIDTH/2) + xr, (HEIGHT/2) - yr]
+
+def rotate(vertex, _t1, _t2, _t3):
+    new_vertex = vertex[:]
+    if _t1:
+        rot_matrix = [
+            [1, 0, 0],
+            [0, np.cos(_t1), -np.sin(_t1)],
+            [0, np.sin(_t1), np.cos(_t1)]
+        ]
+        new_vertex = np.matmul(new_vertex, rot_matrix)
+    if _t2:
+        rot_matrix = [
+            [np.cos(_t2), 0, np.sin(_t2)],
+            [0, 1, 0],
+            [-np.sin(_t2), 0, np.cos(_t2)]
+        ]
+        new_vertex = np.matmul(new_vertex, rot_matrix)
+    if _t3:
+        rot_matrix = [
+            [np.cos(_t3), -np.sin(_t3), 0],
+            [np.sin(_t3), np.cos(_t3), 0],
+            [0, 0, 1]
+        ]
+        new_vertex = np.matmul(new_vertex, rot_matrix)
+    
+    return new_vertex
+
+def draw_line(xy1, xy2):
+    game.draw.line(screen, (255, 255, 255), xy1, xy2)
+
+running = True
+t1 = 0
+t2 = 0.5
+t3 = 0
+dt1 = 0
+dt2 = 0
+dt3 = 0
+# dt1 = np.pi / 1000
+dt2 = np.pi / 1000
+# dt3 = np.pi / 1000
+
+v1x = []
+v1y = []
+
+while running:
+    for event in game.event.get():
+        if event.type == game.QUIT:
+            running = False
+    
+    screen.fill((0, 0, 0))
+
+    for vertex in range(len(vertices)):
+        rotated = rotate(vertices[vertex], t1, t2, t3)
+        v = rotated
+        
+        p = project(v[0], v[1], v[2], focal_len)
+        p_vertices.append(p)
+
+    for line in lines:
+        draw_line(p_vertices[line[0]], p_vertices[line[1]])
+
+    t1 += dt1
+    t2 += dt2
+    t3 += dt3
+    
+    p_vertices = []
+    game.display.update()
